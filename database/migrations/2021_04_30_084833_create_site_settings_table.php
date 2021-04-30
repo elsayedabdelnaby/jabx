fec13700cdbde7b25ad63ac83827339149fc2975
elaraby <elsayed.fcis.1@gmail.com>
2021-04-30 11:03:52 +0200

    ignore vendor in git, create Site Settings model, controller, migration file and seeder

---
+++ database/migrations/2021_04_30_084833_create_site_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('logo');
+            $table->string('favicon')->default('favicon.png');
+            $table->string('meta_title');
+            $table->string('meta_keyword');
+            $table->string('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_settings');
+    }
+}
---
+++ database/seeds/SiteSettingsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\SiteSettings;
+use Illuminate\Database\Seeder;
+
+class SiteSettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $site_settings = new SiteSettings();
+        $site_settings->title = 'jabx';
+        $site_settings->logo = 'logo.png';
+        $site_settings->favicon = 'favicon.png';
+        $site_settings->meta_title = 'Jabx';
+        $site_settings->meta_keyword = 'Jabx';
+        $site_settings->meta_description = 'Jabx';
+        $site_settings->save();
+    }
+}
